86084239d0756441872b872680af0696ad8ed4fb
JakobSchauser <jakob.schauser@gmail.com>
2021-09-21 12:53:01 +0200

    Handed in pop3

--- PoP/Assignment 3/src/3g0.fsx
+++ PoP/Assignment 3/src/3g0.fsx
@@ -11,37 +11,45 @@ let sum (n : int) : int =
     else 0
 
 // Gauss was quite a lot smarter than any of us will ever be. 
-// He said that the following is equal to the "sum" function above and I will trust that blindly!
+// He said that the following is equal to the "sum" function above and we will trust that blindly!
 let simplesum (n: int) : int = if n > 0 then n*(n+1)/2 else 0
 
 
+// Take an input and tries casting it to an integer, which is then returned
 let input () : int =
     printfn "Enter a number to sum to:"
     let x = System.Console.ReadLine()
     x |> int
 
 
-let addwhite (str : string) : string = str + String.replicate (4 - String.length str) " " 
+// We had not seen that there was a built-in padding in F#, so this was our solution
+let addwhite = fun i -> (i |> string) + String.replicate (4 - String.length (i |> string)) " " 
 
 
+// Recursive row-printing with correct whitespace
 let rec printrow (n : int) = 
     if n > 10 then
         true
     else
-        let nstr = n |> string
-        let sstring = sum(n) |> string
-        let ssstring = simplesum(n) |> string
-        let str : string = (addwhite nstr + addwhite sstring + addwhite ssstring)
+        let str : string = addwhite n + addwhite (sum n) + addwhite (simplesum n)
         printfn "%s" str 
         printrow(n+1)
     
 
 
 
+
+// To see if we understand the input system, outcomment the following two lines:
 // let x = input()
-
-printfn "n  |sum|simplesum"
-printrow(1)
+// printfn "%A" (sum x)
 
 
+// To see our solution to the "print in a table"-problem, outcomment the following two lines:
+// printfn "n  |sum|simplesum"
+// printrow(1)
 
+
+// The maximal number is 2^31-1 as we are using integers. 
+// To test this, just outcomment the following two lines 
+// let max : int = 2147483647
+// printfn "%A %A" max (max+1)
