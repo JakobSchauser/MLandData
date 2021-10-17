1e8ac82ee956fe7aeb41a4dd88665f85bb99c505
JakobSchauser <jakob.schauser@gmail.com>
2021-10-17 11:13:52 +0200

    at Absalon

--- PoP/Assignment 6/src/continuedFractionTest.fsx
+++ PoP/Assignment 6/src/continuedFractionTest.fsx
@@ -2,13 +2,13 @@
 open continuedFraction
 
 // Blackbox test:
-// printfn "Black-box testing of cfrac2float" 
+printfn "Black-box testing of cfrac2float" 
 printfn "%5b: The empty list" (cfrac2float [] = 0.0)
 printfn "%5b: [3;4;12;4]" (floor(cfrac2float [3;4;12;4] * 1000.0) = floor(3.244897959 * 1000.0))
 printfn "%5b: Simply [0]" (string(cfrac2float [0]) = "Infinity")
 printfn "%5b: [4;2] = [4;1;1]" (cfrac2float [4;1;1] = cfrac2float [4;2])
 
-// printfn "\nBlack-box testing of float2cfrac" 
+printfn "\nBlack-box testing of float2cfrac" 
 printfn "%5b: 0.0" (float2cfrac 0.0 = [0])
 printfn "%5b: The example 3.245" (float2cfrac 3.245 = [3; 4; 12; 4])
 printfn "%5b: A negative number" (float2cfrac -1.0 = [-1])
@@ -23,9 +23,9 @@ printfn "%5b: A negative number" (float2cfrac -1.0 = [-1])
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
 // | List.length lst | 1a    | 0         | []    | 0.0             |                          |
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
-// |                 | 1b    | 1         | [2]   | 4.5             |                          |
+// |                 | 1b    | 1         | [2]   | 2.0             |                          |
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
-// |                 | 1c    | else      | [4;2] | 4               | + recursive call         |
+// |                 | 1c    | else      | [4;2] | 4.5             | + recursive call         |
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
 // | float2cfrac     | 2     |           |       |                 |                          |
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
@@ -33,4 +33,12 @@ printfn "%5b: A negative number" (float2cfrac -1.0 = [-1])
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
 // |                 | 2b    | true      | 3.245 | [3; 4; 12; 4]   |                          |
 // +-----------------+-------+-----------+-------+-----------------+--------------------------+
+printfn "\nWhite-box tests: "
+printfn "1a : %A" (cfrac2float [])
+printfn "1b : %A" (cfrac2float [2])
+printfn "1c : %A" (cfrac2float [4;2])
+
+// I am not sure how to test this one, the function calls will both do the recursive branch and the break-condition. This tests both:
+printfn "2a : %A" (float2cfrac 3.245)
+
 
