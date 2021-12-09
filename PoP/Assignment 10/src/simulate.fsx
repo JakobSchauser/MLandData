ee0459ff590be53407a5d29003834ca32d5fabda
JakobSchauser <jakob.schauser@gmail.com>
2021-12-09 16:42:48 +0100

    It works!

--- PoP/Assignment 10/src/simulate.fsx
+++ PoP/Assignment 10/src/simulate.fsx
@@ -1,19 +1,21 @@
 type Drone (startx: int, starty : int, destinationx : int, destinationy : int, speed : int) =
     let mutable movedPosition = (startx, starty)
-    let direction = atan2 (float (destinationx - startx)) (float (destinationy - starty))
+    let mutable direction = atan2 (float (destinationy - starty)) (float (destinationx - startx))
 
     member this.Speed = speed
     member this.Position = movedPosition
     member this.Destination = (destinationx,destinationy)
 
     member this.Fly =
-        let xx = (cos direction) * (float speed)
-        let yy = (sin direction) * (float speed)
-        movedPosition <- (int (float (fst movedPosition) + xx), int (float (snd movedPosition) + yy))
+
+        let xx = (cos direction) * (float this.Speed)
+        let yy = (sin direction) * (float this.Speed)
+        movedPosition <- (int (float (fst this.Position) + xx), int (float (snd this.Position) + yy))
+        direction <- atan2 (float (snd this.Destination - snd this.Position)) (float (fst this.Destination - fst this.Position))
 
     member this.AtDestination =
         match movedPosition with
-             | (x,y) -> speed*speed < (destinationx - x) * (destinationx - x) + (destinationy - y) * (destinationy - y)
+             | (x,y) -> speed*speed > (destinationx - x) * (destinationx - x) + (destinationy - y) * (destinationy - y)
 
 
 let dist ((x0, y0) : int*int) ((x1, y1) : int*int) = sqrt (float ((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)))
@@ -46,29 +48,33 @@ type Airspace (a)=
 
     member this.AddRandomDrone (size : int) =
         let rnd = System.Random ()
-        this.AddDrone (rnd.Next(size), rnd.Next(size), rnd.Next(size), rnd.Next(size), rnd.Next(size)/10)
+        this.AddDrone (rnd.Next(size), rnd.Next(size), rnd.Next(size), rnd.Next(size), 10)
 
-    
+    member this.AddRandomDrones (number : int) (size : int) =
+        for i in 1..number do
+            this.AddRandomDrone size
+
     member this.WillCollide (minutes : int) =
         let mutable allcrashes = []
         for i in 0..(minutes*60) do
             this.FlyDrones
             let crashes = findFallenDrones drones [] |> List.filter (fun drones -> 
-                                                               match drones with | (Some d1,Some d2) -> true | _ -> false )
+                                                               match drones with | Some (d1, d2) -> true | _ -> false )
             allcrashes <- allcrashes@crashes
 
-            drones <- List.filter (fun (d1,d2) -> not ((List.contains d1 drones) && (List.contains d2 drones)) )
+            let toremove = List.map (fun dronepair -> match dronepair with | Some (a,_) | Some (_, a) -> a) crashes
+            // allcrashes <- allcrashes@toremove
+            drones <- List.filter (fun drone -> not (List.contains drone toremove)) this.Drones
+        allcrashes
 
 
 
 
 let space = Airspace ()
-space.AddRandomDrone 50
-space.AddRandomDrone 50
-space.AddRandomDrone 50
-space.AddRandomDrone 50
-space.AddRandomDrone 50
-space.AddRandomDrone 50
-space.AddRandomDrone 50
+
+space.AddRandomDrones 20 5000
+printfn "%A" (List.length space.Drones)
 
 printfn "%A" (space.WillCollide 5)
+
+printfn "%A" (List.length space.Drones)
