b958c9f8110305c7de66d52f3f7ab3f82ed76fb0
JakobSchauser <jakob.schauser@gmail.com>
2021-12-09 14:36:10 +0100

    made it work more but not enough

--- PoP/Assignment 10/src/simulate.fsx
+++ PoP/Assignment 10/src/simulate.fsx
@@ -1,22 +1,24 @@
 type Drone (startx: int, starty : int, destinationx : int, destinationy : int, speed : int) =
-    let mutable movedPosition = [startx; starty]
+    let mutable movedPosition = (startx, starty)
     let direction = atan2 (float (destinationx - startx)) (float (destinationy - starty))
 
     member this.Speed = speed
-    member this.Position = (movedPosition.[0], movedPosition.[1])
+    member this.Position = movedPosition
     member this.Destination = (destinationx,destinationy)
 
     member this.Fly =
-        movedPosition <- (movedPositionx.[0] + (cos direction) * speed, movedPosition.[1] + (sin direction) * speed)
+        let xx = (cos direction) * (float speed)
+        let yy = (sin direction) * (float speed)
+        movedPosition <- (int (float (fst movedPosition) + xx), int (float (snd movedPosition) + yy))
 
     member this.AtDestination =
         match movedPosition with
              | (x,y) -> speed*speed < (destinationx - x) * (destinationx - x) + (destinationy - y) * (destinationy - y)
 
 
-let dist ((x0, y0) : int*int) ((x1, y1) : int*int) = sqrt ((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1))
+let dist ((x0, y0) : int*int) ((x1, y1) : int*int) = sqrt (float ((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)))
 
-type Airspace =
+type Airspace (a)=
     let mutable drones = []
 
     let rec findFallenDrones (drones) (fallendrones) =
@@ -24,7 +26,7 @@ type Airspace =
             | head::tail -> 
                 let newfallendrones = tail |> List.map (fun (d : Drone) -> 
                                                     match (d.AtDestination,head.AtDestination) with
-                                                        | (false,false) -> if dist head d < 500 then Some (d,head) else None
+                                                        | (false,false) -> if dist head.Position d.Position < 500.0 then Some (d,head) else None
                                                         | _ -> None
                                                         )
                 findFallenDrones tail (fallendrones@newfallendrones)
@@ -39,7 +41,7 @@ type Airspace =
         this.Drones |> List.iter (fun (d : Drone) -> if not d.AtDestination then d.Fly)
 
     member this.AddDrone (startx: int, starty : int, destinationx : int, destinationy : int, speed : int) = 
-        let newdrone = Drone startx starty destinationx destinationy speed
+        let newdrone = Drone (startx, starty, destinationx, destinationy, speed)
         drones <- newdrone::drones
 
     member this.AddRandomDrone (size : int) =
@@ -52,7 +54,7 @@ type Airspace =
         for i in 0..(minutes*60) do
             this.FlyDrones
             let crashes = findFallenDrones drones [] |> List.filter (fun drones -> 
-                                                               match drones with |(d1,d2) -> true | _ -> false )
+                                                               match drones with | (Some d1,Some d2) -> true | _ -> false )
             allcrashes <- allcrashes@crashes
 
             drones <- List.filter (fun (d1,d2) -> not ((List.contains d1 drones) && (List.contains d2 drones)) )
