1340a8533b93b501807a10df645bf2bfbce40efd
JakobSchauser <jakob.schauser@gmail.com>
2021-12-11 12:39:04 +0100

    Only missing some tests and a readme

--- PoP/Assignment 10/src/simulate.fs
+++ PoP/Assignment 10/src/simulate.fs
@@ -5,57 +5,99 @@ type Drone (startx: int, starty : int, destinationx : int, destinationy : int, s
     let mutable direction = atan2 (float (destinationy - starty)) (float (destinationx - startx))
 
     member this.Speed = speed
+    member this.Id = sprintf "%i%i%i%i%i" startx starty destinationx destinationy speed
     member this.Position = movedPosition
     member this.Destination = (destinationx,destinationy)
 
+    /// <summary> Calculates and executes one time step of flight </summary>
+    /// <returns> Nothing </returns>
     member this.Fly =
-
         let xx = (cos direction) * (float this.Speed)
         let yy = (sin direction) * (float this.Speed)
         movedPosition <- (int (float (fst this.Position) + xx), int (float (snd this.Position) + yy))
         direction <- atan2 (float (snd this.Destination - snd this.Position)) (float (fst this.Destination - fst this.Position))
 
+    /// <summary> Checks whether the drone is at its destination </summary>
+    /// <returns> True or false </returns>
     member this.AtDestination =
         match movedPosition with
              | (x,y) -> speed*speed > (destinationx - x) * (destinationx - x) + (destinationy - y) * (destinationy - y)
 
 
+/// <summary> Calculates the distance between two points </summary>
+/// <param name = (x0,y0))> The first point </param>
+/// <param name = (x0,y0))> The second point </param>
+/// <returns> The distance as a float </returns>
 let dist ((x0, y0) : int*int) ((x1, y1) : int*int) = sqrt (float ((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)))
 
-type Airspace (a)=
+type Airspace (size : int) =
+
     let mutable drones = []
 
+    /// <summary> Recursive function that finds all drones that collide </summary>
+    /// <param name = drones> The list of drones to consider </param>
+    /// <param name = fallendrones> The collided drones found so far </param>
+    /// <returns> A list of collided drones as Some(drone1,drone2) or None </returns>
     let rec findFallenDrones (drones) (fallendrones) =
         match drones with
             | head::tail -> 
                 let newfallendrones = tail |> List.map (fun (d : Drone) -> 
                                                     match (d.AtDestination,head.AtDestination) with
                                                         | (false,false) -> if dist head.Position d.Position < 500.0 then Some (d,head) else None
-                                                        | _ -> None
-                                                        )
+                                                        | _ -> None)
                 findFallenDrones tail (fallendrones@newfallendrones)
             | [] -> fallendrones
 
+    member this.Size = size    
     member this.Drones = drones
 
+    /// <summary> Finds the distance between two drones </summary>
+    /// <param name = drone1> The first drone </param>
+    /// <param name = drone2> The second drone </param>
+    /// <returns> The distance between the drones as a float </returns>
     member this.DroneDist (drone1 : Drone) (drone2 : Drone) = 
         dist drone1.Position drone2.Position
 
+    /// <summary> Lets all the drones in the airspace "fly" </summary>
+    /// <returns> Nothing </returns>
     member this.FlyDrones =
         this.Drones |> List.iter (fun (d : Drone) -> if not d.AtDestination then d.Fly)
 
+    /// <summary> Adds a new drone to the airspace </summary>
+    /// <param name = startx> The starting x-position </param>
+    /// <param name = starty> The starting y-position </param>
+    /// <param name = destinationx> The destinations x-position </param>
+    /// <param name = destinationy> The destinations y-position </param>
+    /// <param name = speed> The speed of the drone </param>
+    /// <returns> Notihng  </returns>
     member this.AddDrone (startx: int, starty : int, destinationx : int, destinationy : int, speed : int) = 
         let newdrone = Drone (startx, starty, destinationx, destinationy, speed)
         drones <- newdrone::drones
 
-    member this.AddRandomDrone (size : int) =
+    /// <summary> Adds a new drone with a random starting position and destination </summary>
+    /// <returns> Nothing  </returns>
+    member this.AddRandomDrone =
         let rnd = System.Random ()
-        this.AddDrone (rnd.Next(size), rnd.Next(size), rnd.Next(size), rnd.Next(size), 10)
+        this.AddDrone (rnd.Next(this.Size), rnd.Next(this.Size), rnd.Next(this.Size), rnd.Next(this.Size), 10)
 
-    member this.AddRandomDrones (number : int) (size : int) =
+    /// <summary> Adds N new drones with a random starting positions and destinations </summary>
+    /// <param name = number> The number of drones to add </param>
+    /// <returns> Nothing  </returns>
+    member this.AddRandomDrones (number : int) =
         for i in 1..number do
-            this.AddRandomDrone size
+            this.AddRandomDrone
 
+    /// <summary> Gets the drone ids in the airspace </summary>
+    /// <returns> A list of drone ids </returns>
+    member this.GetDrones =  List.map (fun (drone : Drone) -> drone.Id) this.Drones 
+
+    /// <summary> Gets the drone ids in the airspace </summary>
+    /// <returns> A list of drone ids </returns>
+    member this.GetDronePositions =  List.map (fun (drone : Drone) -> drone.Position) this.Drones 
+
+    /// <summary> Finds all the collisions that happen within the next N minutes </summary>
+    /// <param name = minuts> The number of minutes to simulate </param>
+    /// <returns> The list of collisions that happened </returns>
     member this.WillCollide (minutes : int) =
         let mutable allcrashes = []
         for i in 0..(minutes*60) do
@@ -65,7 +107,7 @@ type Airspace (a)=
             allcrashes <- allcrashes@crashes
 
             let toremove = List.map (fun dronepair -> match dronepair with | Some (a,_) | Some (_, a) -> a) crashes
-            // allcrashes <- allcrashes@toremove
+
             drones <- List.filter (fun drone -> not (List.contains drone toremove)) this.Drones
         allcrashes
 
--- PoP/Assignment 10/src/testSimulation.fsx
+++ PoP/Assignment 10/src/testSimulation.fsx
@@ -1,11 +1,71 @@
 #load "simulate.fs"
 open Simulation
 
-let space = Airspace ()
 
-space.AddRandomDrones 20 5000
-printfn "%A" (List.length space.Drones)
+let space = Airspace (50*100)
 
-printfn "%A" (space.WillCollide 5)
 
-printfn "%A" (List.length space.Drones)
+
+//test
+let drone = Drone (0, 0, 1000, 1000, 10) 
+let droneAtDestination = Drone (0, 0, 0, 0, 10) 
+let droneZeroSpeed = Drone (0, 0, 1000, 1000, 0) 
+
+// Fly
+let posbefore = drone.Position
+drone.Fly
+printfn "%b" (posbefore <> drone.Position)
+
+let zeroposbefore = droneZeroSpeed.Position
+droneZeroSpeed.Fly
+printfn "%b" (zeroposbefore = droneZeroSpeed.Position)
+
+// AtDestination
+printfn "%b" (droneAtDestination.AtDestination)
+
+printfn "%b" (not drone.AtDestination)
+
+
+// AddDrone
+printfn "%b" (List.length space.Drones = 0)
+space.AddDrone (0, 0, 1000, 1000, 10) 
+printfn "%b" (List.length space.Drones = 1)
+
+// AddRandomDrone
+let mutable dronesbefore = space.GetDrones
+space.AddRandomDrone
+let mutable dronesafter = space.GetDrones
+printfn "%b" (List.length dronesbefore < List.length dronesafter)
+
+
+// AddRandomDrones
+dronesbefore <- space.GetDrones
+space.AddRandomDrones 20
+dronesafter <- space.GetDrones
+printfn "%b" (List.length dronesbefore + 20 = List.length dronesafter)
+
+
+// DroneDist
+printfn "%b" (space.DroneDist droneAtDestination droneZeroSpeed = 0.0)
+printfn "%b" (space.DroneDist droneAtDestination drone > 0.0)
+
+// WillCollide
+let mutable emptyspace = Airspace (50*100)
+let collided = emptyspace.WillCollide 10
+printfn "%b" (List.length collided = 0)
+
+emptyspace.AddRandomDrones 50
+let collidednow = emptyspace.WillCollide 10
+printfn "%b" (List.length collidednow > 0)
+
+// FlyDrones
+emptyspace <- Airspace (50*100)
+emptyspace.AddRandomDrones 50
+
+let droneposbefore = emptyspace.GetDronePositions
+emptyspace.FlyDrones
+emptyspace.FlyDrones
+emptyspace.FlyDrones
+
+let allchanged = List.exists2 (fun ((a,b) :int*int) ((c,d) :int*int) -> a = c && b = d) droneposbefore emptyspace.GetDronePositions
+printfn "%b" (not allchanged)
