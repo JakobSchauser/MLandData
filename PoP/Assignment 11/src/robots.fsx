8f3cfb4744855511ca6df9574e7691e55cc9f9c5
JakobSchauser <jakob.schauser@gmail.com>
2022-01-02 00:44:18 +0100

    bad assignments deserve bad solutions

--- PoP/Assignment 11/src/robots.fsx
+++ PoP/Assignment 11/src/robots.fsx
@@ -1,16 +1,40 @@
- type BoardDisplay (rows:int, cols:int) =
-    let mutable board = [for i in 1 .. rows*cols -> "  "]
 
-    // "  " = empty
-    // "AA"-"ZZ" = robot
-    // b = bottom wall
-    // r = right wall
-    member.Board = board
-    member.Set (row:int) (col:int) (cont:string) =
-        
+type Square = {on : string; right_wall : bool; bottom_wall : bool}
+type BoardDisplay (rows:int, cols:int) =
+   let mutable board = [for i in 1 .. rows*cols -> {"  ";false;false} ]
 
-    member SetBottomWall : row:int * col:int -> unit
-    member SetRightWall : row:int * col:int -> unit
-    member.Show =
+   // "" = empty
+   // "A"-"Z" = robot
+   // r = right wall
+   // b = bottom wall
 
-    end
+   member.Board = board
+   member.Set (row:int) (col:int) (cont:string) =
+      
+
+   member SetBottomWall : row:int * col:int -> unit
+   member SetRightWall : row:int * col:int -> unit
+   member.Show =
+      let rec makerow (row1:string) (row2:string) (boardrow:list) =
+         match boardrow with
+            | head::tail -> 
+               let mutable newstr = head.on
+               if head.right_wall then
+                  newstr <- newstr + "|"
+               else
+                  newstr <- newstr + " "
+               if head.bottom_wall then
+                  let newstrl2 = "--+"
+               else
+                  let newstrl2 = "  +"
+
+               makerow (row1 + newstr) (row2 + newstrl2) tail
+            | [] -> (row1, row2)
+
+      for row in 0..(rows-2) do
+         let rows = makerow "" "" board.[i*cols:(i+1)*cols]
+         printfn "%A" fst rows
+         printfn "%A" snd rows
+      end
+
+   end
